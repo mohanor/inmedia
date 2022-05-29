b54b4663bcb0da91dad493d5bc7a78f2e9800643
Mohammed Atef <matef@e1r6p1.1337.ma>
2022-05-29 13:02:56 +0100

    webp

--- js/main.js
+++ js/main.js
@@ -124,3 +124,22 @@ $('.nav-link').on('click', function () {
 
     icons[0].classList.toggle("open");
 })
+
+
+/** scrolling web site */
+
+window.addEventListener("scroll", reveal);
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+/** */
